38bbc316607b2a69d7724765a04058ce05304510
sukitten <richardperez4840964@gmail.com>
2024-05-30 08:05:26 +0200

    enhance readability

--- Program.cs
+++ Program.cs
@@ -14,13 +14,17 @@ namespace YourNamespace
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureWebHostDefaults(webBuilder => 
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .ConfigureServices((context, services) =>
+                .ConfigureServices((context, services) => 
                 {
+                    // Read environment variable
                     var myEnvVariable = Environment.GetEnvironmentVariable("MY_ENV_VARIABLE");
+
+                    // Potentially use the environment variable here 
+                    // or pass it to services as needed
                 });
     }
 
@@ -28,10 +32,27 @@ namespace YourNamespace
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // Here, you will add services to the container.
+            // For example, to add support for controllers:
+            // services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // This is where you'd configure the HTTP request pipeline.
+            
+            // Example middleware usage:
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // 
+            // app.UseRouting();
+            //
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapControllers();
+            // });
         }
     }
 }
