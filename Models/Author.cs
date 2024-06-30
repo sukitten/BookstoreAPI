d83883ebd61f81e0945d639552c23e6e7f17b618
sukitten <richardperez4840964@gmail.com>
2024-06-30 18:41:06 +0200

    Develop Author.cs

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyApplication.Models
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+    }
+}
