f28d3f0363ee6286945c34ec28339a96af7ab516
sukitten <richardperez4840964@gmail.com>
2024-07-02 23:18:46 +0200

    optimize

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,46 @@
+public class AuthorService
+{
+    private readonly HttpClient _httpClient;
+
+    public AuthorService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<AuthorWithBooks> GetAuthorDetailsWithBooksAsync(Guid authorId)
+    {
+        var response = await _httpClient.GetAsync($"api/authors/{authorId}/withBooks");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var authorDetails = JsonConvert.DeserializeObject<AuthorWithBooks>(content);
+            return authorDetails;
+        }
+
+        throw new Exception("Failed to fetch author details with books.");
+    }
+}
+```
+
+```csharp
 using System;
+using System.Collections.Generic;
 
 namespace MyApplication.Models
 {
-    public class Author
+    public class AuthorWithBooks
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Biography { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        // Other book-related properties...
     }
 }
