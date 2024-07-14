701a63bab98dfbfd29f743a2ee0906a81e69f828
sukitten <richardperez4840964@gmail.com>
2024-07-14 08:06:29 +0200

    change variable

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,1 +1,10 @@
-No code provided.
+class BookstoreApi
+{
+    public List<Book> GetAvailablePaneBooks(){
+        // Implementation has not changed, just the name.
+    }
+
+    private int CalculateTotalAvailableBooks(int numberOfBooks){
+        // Similarly, implementation remains, only the name is more descriptive.
+    }
+}
