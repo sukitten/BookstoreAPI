11651b08357df169a986ac8909ecba20ee4bb0ea
sukitten <richardperez4840964@gmail.com>
2024-07-20 17:42:00 +0200

    improve variables

--- Data/BookstoreContext.cs
+++ Data/BookstoreContext.cs
@@ -1,20 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 public class Book
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string ISBN { get; set; }
-    public List<Author> Authors { get; set; } = new List<Author>();
-    public List<Order> Orders { get; set; } = new List<Order>();
+    public string Isbn { get; set; } // Changed from ISBN for consistency with C# naming conventions
+    public List<Author> Authors { get; set; } = new();
+    public List<Order> Orders { get; set; } = new();
 }
 
 public class Author
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<Book> Books { get; set; } = new List<Book>();
+    public List<Book> Books { get; set; } = new();
 }
 
 public class Order
@@ -31,7 +32,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Author> Authors { get; set; }
     public DbSet<Order> Orders { get; set; }
 
-    protected override void OnConfigrawing(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // Corrected Method Name
     {
         var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
         optionsBuilder.UseSqlServer(connectionString);
@@ -40,7 +41,7 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Book>()
-            .HasMany(b => b.Authors)
-            .WithMany(a => a.Books);
+            .HasMany(book => book.Authors) // Changed parameter name for clarity
+            .WithMany(author => author.Books); // Changed parameter name for clarity
     }
 }
